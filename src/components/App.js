1fea7eff8ac23299d755bd0812a2063a7d97c797
nenjotsu <renenjo.valente@ubidy.com>
2018-09-29 11:55:15 +0800

    GEN-master Changes: initial

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import DevTools from './DevTools'
+import Sidebar from './Sidebar'
+import Switcher from './Switcher'
+
+import styles from '../css/App'
+
+const App = () => (
+  <div>
+    <div className={styles.app}>
+      <Sidebar />
+      <Switcher />
+    </div>
+    <DevTools />
+  </div>
+)
+
+export default App
